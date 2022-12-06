6cb85d030cfa7184dfce8a4d6151ed9384d3baae
Andrey Soroko <supersoroko@gmail.com>
2022-12-06 22:28:03 +0300

    start project

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+import { Todolist } from "./componets/Todolist";
+
+export const App = () => {
+  const tasks1 = [
+    { id: 1, title: "HTML&CSS", isDone: true },
+    { id: 2, title: "JS", isDone: true },
+    { id: 3, title: "ReactJS", isDone: false },
+  ];
+  const tasks2 = [
+    { id: 1, title: "Hello world", isDone: true },
+    { id: 2, title: "I am Happy", isDone: false },
+    { id: 3, title: "Yo", isDone: false },
+  ];
+
+  return (
+    <div className="App">
+      <Todolist />
+    </div>
+  );
+};
